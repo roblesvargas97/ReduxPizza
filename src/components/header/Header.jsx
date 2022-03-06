408ae13cc0ce3accfef94d5d8bc841144d70a07b
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-06 15:32:22 -0600

    :heavy_plus_sign: Add React router dom dependency
    Add react router dom dependency

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>
-            <p>Hola</p>
+        <header className='w-full h-[15vh] bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 backdrop-blur-sm border-b-2 border-blue-500 hidden lg:flex'>
+            <img className='l w-40 h-24' src="https://i.imgur.com/KVwBOVw.png" alt="logo"/>
+            
         </header>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     content: ['./dist/*.html','./src/**/*.{js,jsx,ts,tsx}','./public/*.html'],
     theme: {
-      extend: {},
+      extend: {
+        colors:  {
+          'dark-blue':'#26324F',
+          'dark-blue-1': 'rgba(17, 54, 140, 0.93)'
+        },
+      },
     },
     plugins: [],
   }
+
