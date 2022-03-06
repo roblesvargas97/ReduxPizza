1ff8b8d8c8269040b7f423e9ef84148728ff061c
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-06 12:18:25 -0600

    :tada: First Commit
    First Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <p>Hola</p>
+        </header>
+    );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ['./dist/*.html','./src/**/*.{js,jsx,ts,tsx}','./public/*.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
