e5cfe30cd2e03e0ca2cc1f823c25bb9a37ef5acd
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-14 22:19:59 -0600

    :sparkles: Add animation banner
    Add animation banner

--- src/components/BannerText/BannetText.jsx
+++ src/components/BannerText/BannetText.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const BannetText = () => {
     return (
-        <div className='flex flex-col items-center overflow-hidden w-full h-12 bg-red-500 '>
-            {/* <h2 className=' font-bold text-5xl text-white text-center'>!El mejor </h2> */}
-            <div className='letterA flex flex-col items-center w-full' >
-                <span className=' font-bold text-5xl text-white'> Sabor </span>
-                <span className=' font-bold text-5xl text-white'> Calidad </span>
-                <span className=' font-bold text-5xl text-white'> Precio </span>
+
+        <div className='flex flex-col space-y-5 lg:flex-row lg:space-y-0 lg:space-x-5'>
+            <h2 className='font-bold text-5xl w-full text-white'>!El Mejor</h2>
+            <div className='flex flex-col items-center overflow-hidden w-full h-12  '>
+                <div className='letterA flex flex-col items-center w-full mt-10 mb-10' >
+                    <span className=' font-bold text-5xl text-white'> Sabor¡ </span>
+                    <span className=' font-bold text-5xl text-white'> Calidad¡ </span>
+                    <span className=' font-bold text-5xl text-white'> Precio¡ </span>
+                </div>
             </div>
         </div>
     );
