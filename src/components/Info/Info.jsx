52b19ac5a6930bcc9b89e2c0ce5b3ed601ec11da
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-28 18:40:35 -0600

    :adhesive_bandage: Add Ripple to InfoItem and BUtton Menu
    Add Ripple to InfoItem and Button Menu

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -22,7 +22,10 @@ const Info = () => {
     return (
         <div className=" relative w-full min-h-[45vh] md:min-h-[35vh] lg:min-h-[45vh] flex flex-col items-center p-5 md:flex-row bg-white space-y-5 lg:p-10 md:justify-center md:items-center md:space-x-5 md:space-y-0 ">
             {infoArray.map((element , index) => <InfoItem key={index} icon={element.icon} text={element.text} />)}
-            <button className=' absolute top-[-72px] md:top-[-48px] rounded-full   ml-0 w-52 h-16 border-[3px] border-dark-blue flex justify-between items-center font-bold  py-1 px-10 text-xl bg-gradient-to-r from-sky-300 via-sky-100 to-white text-dark-blue hover:scale-105 transition-all' >Menu <FaChevronDown/> </button>
+            <button
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="dark" 
+                className=' absolute top-[-72px] md:top-[-48px] rounded-full   ml-0 w-52 h-16 border-[3px] border-dark-blue flex justify-between items-center font-bold  py-1 px-10 text-xl bg-gradient-to-r from-sky-300 via-sky-100 to-white text-dark-blue hover:scale-105 transition-all' >Menu <FaChevronDown/> </button>
         </div>
     );
 }
--- src/components/InfoItem/InfoItem.jsx
+++ src/components/InfoItem/InfoItem.jsx
@@ -3,7 +3,11 @@ import { FaPizzaSlice ,FaHandPointRight , FaMotorcycle } from 'react-icons/fa';
 
 const InfoItem = ({icon , text}) => {
     return (
-        <article className=' w-full md:w-80 h-60  text-gray-500 flex flex-col items-center justify-between border-2 border-gray-500  shadow-md lg:border-gray-400 lg:text-gray-400 lg:hover:text-gray-500 lg:hover:border-gray-500 shadow-gray-200 p-10 rounded-lg hover:shadow-xl hover:shadow-gray-500  transition-all ' >
+        <article
+
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="dark"
+            className=' w-full md:w-80 h-60 cursor-pointer  text-gray-500 flex flex-col items-center justify-between border-2 border-gray-500  shadow-md lg:border-gray-400 lg:text-gray-400 lg:hover:text-gray-500 lg:hover:border-gray-500 shadow-gray-200 p-10 rounded-lg hover:shadow-xl hover:shadow-gray-500  transition-all ' >
             {icon==='FaPizzaSlice' && <FaPizzaSlice className=' text-7xl' /> }
             {icon==='FaHandPointRight' && <FaHandPointRight className=' text-7xl' /> }
             {icon==='FaMotorcycle' && <FaMotorcycle className=' text-7xl' /> }
