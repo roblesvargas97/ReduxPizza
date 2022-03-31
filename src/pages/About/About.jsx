3f9f9468c466e17aeec6a219675a1f109991e5cc
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-30 21:40:09 -0600

    :iphone: Responsive tablets about
    Responsive tablets about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,33 +1,53 @@
 import React from "react";
 import pizzaAbout from "../../assets/images/pizzaAbout";
+import PointsOrnament from "../../components/PointsOrnament/PointsOrnament";
+import SwirlOrnament from "../../components/SwirlOrnament/SwirlOrnament";
 
 const About = () => {
   return (
-    <div className="w-full min-h-[85vh] overflow-hidden p-5 bg-white flex flex-col lg:flex-row justify-center space-y-5 lg:space-y-0 ">
-      <section className=" w-[50%] flex flex-col items-center lg:items-start space-y-2" >
-        <h2 className=" text-4xl md:text-7xl lg:text-6xl text-dark-blue font-bold text-center lg:text-left ">
+    <div className=" w-full min-h-[85vh] overflow-hidden p-5 bg-white flex flex-col md:items-center lg:flex-row justify-center space-y-5 lg:space-y-0 ">
+      <section className=" relative lg:w-[50%] flex flex-col items-center lg:items-start space-y-2">
+        <SwirlOrnament
+          styles=" absolute lg:h-96 lg:top-[-75%] lg:left-[50%] md:left-[15%] md:top-[-100%]  hidden md:block  "
+          fillColor="#92b1fd"
+        />
+        <PointsOrnament
+          styles=" absolute h-60 lg:bottom-[-10%] lg:left-[0] md:bottom-[-150%] md:left-[0]  hidden md:block  "
+          fillColor="#92b1fd"
+        />
+        <PointsOrnament
+          styles=" absolute h-60 lg:bottom-[0] lg:right-[-100%] md:bottom-[-150%] md:right-[0] hidden md:block  "
+          fillColor="#92b1fd"
+        />
+        <h2 className=" text-4xl z-10 md:text-7xl lg:text-6xl text-dark-blue font-bold text-center lg:text-left ">
           Historia
         </h2>
-        <p className="w-[90%] text-xl md:text-3xl lg:text-2xl text-dark-blue font-normal text-center lg:text-left">
+        <p className="w-[90%] h- z-10 text-xl md:text-3xl lg:text-2xl text-dark-blue font-normal text-center lg:text-left">
           Redux pizza nacio con la finalidad de poner a prueba mis conocimientos
           de tailwind , react js y redux.
         </p>
-        <img className=" w-60 lg:w-80 self-center" src={pizzaAbout} alt="Pizza" />
+        <img
+          className=" w-60 lg:w-80 z-10 self-center"
+          src={pizzaAbout}
+          alt="Pizza"
+        />
       </section>
-      <section className=" space-y-2 w-[50%]">
+      <section className=" space-y-2 lg:w-[50%] z-10">
         <h2 className=" text-4xl md:text-7xl lg:text-6xl text-dark-blue font-bold text-center ">
           Informacion
         </h2>
-        <article className=" space-y-2" >
+        <article className=" space-y-2">
           <h3 className="text-2xl md:text-5xl lg:text-2xl text-dark-blue font-bold text-left">
             Ubicacion:
           </h3>
-          <p className="text-xl md:text-3xl lg:text-xl font-normal text-center">
+          <p className="text-xl md:text-3xl lg:text-xl text-dark-blue font-normal text-center">
             Av 16 de Septiembre 120 B, San Juan, 54960 Tultepec, Méx.
           </p>
         </article>
         <article className=" space-y-2">
-          <h3 className="text-2xl md:text-5xl lg:text-2xl text-dark-blue font-bold text-left">Dias:</h3>
+          <h3 className="text-2xl md:text-5xl lg:text-2xl text-dark-blue font-bold text-left">
+            Dias:
+          </h3>
           <div className=" flex justify-between">
             <div className=" h-10 w-10 md:h-16 md:w-16 lg:h-10 lg:w-10 flex justify-center items-center rounded-full bg-dark-blue text-xl md:text-3xl lg:text-xl font-bold text-white shadow-sm shadow-black">
               L
@@ -56,7 +76,7 @@ const About = () => {
           <h3 className="text-2xl md:text-5xl lg:text-2xl text-dark-blue font-bold text-left">
             Horarios de Atencion:
           </h3>
-          <p className="text-xl md:text-3xl lg:text-xl font-normal text-left">
+          <p className="text-xl md:text-3xl lg:text-xl text-dark-blue font-normal text-left">
             11:00 de la mañana <br />
             10:00 de la noche.
           </p>
--- src/pages/PizzaMenu/PizzaMenu.jsx
+++ src/pages/PizzaMenu/PizzaMenu.jsx
@@ -6,8 +6,14 @@ import SwirlOrnament from "../../components/SwirlOrnament/SwirlOrnament";
 const PizzaMenu = () => {
   return (
     <div className=" h-[85vh] overflow-hidden flex flex-col lg:flex-row lg:justify-between lg:items-center relative p-5 bg-gradient-to-r space-y-5 from-dark-blue via-dark-blue to-dark-blue-1">
-      <SwirlOrnament styles=" hidden opacity-90 lg:block absolute w-96 top-[-60%] left-[-15%] " />
-      <PointsOrnament styles=" hidden opacity-90 lg:block absolute w-40 bottom-[-9%] left-0 " />
+      <SwirlOrnament
+        styles=" hidden opacity-90 lg:block absolute w-96 top-[-60%] left-[-15%] "
+        fillColor="white"
+      />
+      <PointsOrnament
+        styles=" hidden opacity-90 lg:block absolute w-40 bottom-[-9%] left-0 "
+        fillColor="white"
+      />
       <h2 className=" text-4xl md:text-7xl font-bold text-white text-center ">
         Nuestro Menu
       </h2>
