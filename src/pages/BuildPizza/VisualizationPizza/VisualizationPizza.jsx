cbfe7025b88225ec4696a16ba88b8044d9f1c908
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-01 20:12:33 -0600

    :sparkles: VisualizationPizza Component
    VisalizationPizza Component

---
+++ src/pages/BuildPizza/VisualizationPizza/VisualizationPizza.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Masa from "../../../assets/images/masa.png";
+
+const VisualizationPizza = () => {
+  return (
+    <div>
+      <img src={Masa} alt="masa pizza" />
+    </div>
+  );
+};
+
+export default VisualizationPizza;
