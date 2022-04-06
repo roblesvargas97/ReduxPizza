434ad4e86b90bccfd2c87477d167d022513a0365
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-05 20:56:47 -0500

    :construction: changes
    c

--- src/components/ShoppingCart/BackGroundShoppingCart.jsx
+++ src/components/ShoppingCart/BackGroundShoppingCart.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setShowShoppingCart } from "../../Slices/uiSlice";
 
 const BackGroundShoppingCart = () => {
-    return (
-        <div className=" bg-black/20 animate-rotateAndShow2" >
+  const dispatch = useDispatch();
 
-        </div>
-    );
-}
+  const handleClick = (e) => {
+    dispatch(setShowShoppingCart(false));
+  };
+
+  return (
+    <div
+      onClick={(e) => handleClick(e)}
+      className=" fixed hidden lg:block top-[15VH] left-0  w-full h-[85vh] bg-dark-blue/50 animate-rotateAndShow2 z-20"
+    ></div>
+  );
+};
 
 export default BackGroundShoppingCart;
