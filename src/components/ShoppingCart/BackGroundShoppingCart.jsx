e926214a1a20c9f03917f2f906f974439416b0c7
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-05 20:33:43 -0500

    :sparkles: create BackgroundShoppingCart

---
+++ src/components/ShoppingCart/BackGroundShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BackGroundShoppingCart = () => {
+    return (
+        <div className=" bg-black/20 animate-rotateAndShow2" >
+
+        </div>
+    );
+}
+
+export default BackGroundShoppingCart;
