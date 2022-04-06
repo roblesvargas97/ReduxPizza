85442a8c31374c81c5b6eeaf374401593b9918a6
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-05 19:54:13 -0500

    :lipstick: Styles ShoppingCartItem
    Styles ShoppingCartItem

--- src/components/ShoppingCart/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/components/ShoppingCart/ShoppingCartItem/ShoppingCartItem.jsx
@@ -21,9 +21,11 @@ const ShoppingCartItem = ({ info }) => {
   const formatIngredients = separateArrayByComma(info.ingredients);
 
   return (
-    <article className="w-full h-20 flex items-center justify-center border-2 border-blue overflow-hidden rounded-lg bg-dark-blue-1 text-dark-blue ">
-      <img className="w-[20%]" src={info.img} alt={info.name} />
-      <div className="w-[70%] flex flex-col justify-center overflow-hidden h-full text-white bg-dark-blue ">
+    <article className="w-full h-20 flex items-center justify-center border-2 border-white overflow-hidden rounded-lg bg-dark-blue text-dark-blue shadow shadow-white/50 ">
+      <div className="w-[20%] h-full bg-gradient-to-tr from-sky-500 via-sky-200 to-white ">
+        <img className="w-full" src={info.img} alt={info.name} />
+      </div>
+      <div className="w-[70%] flex flex-col justify-center overflow-hidden h-full text-white bg-gradient-to-tl from-sky-500 via-transparent to-transparent ">
         <h3 className=" text-center text-sm">{info.name}</h3>
         <div className=" custom-scroll h-6 rounded-lg w-full overflow-y-scroll">
           <ul className=" list-disc pl-5 text-white ">
@@ -47,7 +49,7 @@ const ShoppingCartItem = ({ info }) => {
         </div>
       </div>
       <div className="w-[10%] h-full flex justify-center items-center ">
-        <button className=" w-full h-full rounded-l-none bg-sky-200 w-100% text-dark-blue font-bold text-xl rounded-sm">
+        <button className=" w-full h-full rounded-l-none bg-dark-blue w-100% text-white font-bold text-xl rounded-sm">
           x
         </button>
       </div>
