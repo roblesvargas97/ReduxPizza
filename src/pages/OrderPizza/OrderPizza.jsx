cb67fe781b43b6a04ed1ac8a2ff11d6f3dcb0403
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-11 18:17:12 -0500

    :construction: order pizza work
    order pizza work

--- src/pages/OrderPizza/OrderPizza.jsx
+++ src/pages/OrderPizza/OrderPizza.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import OrderPizzaItem from "./OrderPizzaItem/OrderPizzaItem";
 
 const OrderPizza = () => {
-    return (
-        <div>
-            <h2>Hola como estas</h2>
+  const shoppingCartState = useSelector(
+    (state) => state.pizza.pizzaShoppingCart
+  );
+
+  const shoppingCartItemsPrice = shoppingCartState.reduce(
+    (a, b) => a + Number(b.price * b.quantity),
+    0
+  );
+
+
+  return (
+    <div className="p-5 h-[85vh] bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
+      <h2 className=" text-4xl md:text-7xl lg:text-6xl font-bold text-center text-white">
+        Revisa tu pedido
+      </h2>
+      <div className=" relative h-[60vh] w-full bg-white-glass border-2 border-blue rounded-lg  ">
+        <div className=" h-[58vh] space-y-2 overflow-y-scroll p-2" >
+          {shoppingCartState.map((element, index) => (
+            <OrderPizzaItem key={index} info={element} />
+          ))}
         </div>
-    );
-}
+        <div className=" p-5 flex flex-col rounded-lg justify-center items-end absolute bottom-0 left-0 w-full h-10 bg-dark-blue text-white">
+          <p className="">Precio Total: {shoppingCartItemsPrice} MXN </p>
+        </div>
+      </div>
+      <button className=" bg-sky-200 rounded-lg  px-3 py-2 text-xl lg:text-lg font-bold text-dark-blue hover:scale-105 transition-all">
+        Realizar compra
+      </button>
+    </div>
+  );
+};
 
 export default OrderPizza;
---
+++ src/pages/OrderPizza/OrderPizzaItem/OrderPizzaItem.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useDispatch , useSelector } from "react-redux";
+import "../styles.css";
+
+const OrderPizzaItem = ({ info }) => {
+  const dispatch = useDispatch();
+
+  const separateArrayByComma = (array) => {
+    const stringArray = array.join(", ");
+    const lastCommaFromArray = stringArray.lastIndexOf(", ");
+    if (lastCommaFromArray !== -1) {
+      return (
+        stringArray.substring(0, lastCommaFromArray) +
+        " y" +
+        stringArray.substring(lastCommaFromArray + 1) +
+        "."
+      );
+    } else {
+      return stringArray;
+    }
+  };
+
+  return (
+    <article className="w-full h-44 flex items-center justify-center border-2 border-white overflow-hidden rounded-lg bg-dark-blue text-dark-blue shadow shadow-white/50 ">
+      <div className="w-[40%] flex justify-center items-center h-full bg-gradient-to-tr from-sky-500 via-sky-200 to-white ">
+        <img className="w-full" src={info.img} alt={info.name} />
+      </div>
+      <div className=" relative w-[60%] flex flex-col space-y-2 p-2 justify-center overflow-hidden h-full text-white bg-gradient-to-tl from-sky-500 via-transparent to-transparent ">
+        <h3 className=" text-center text-base font-bold">{info.name}</h3>
+        <div className=" custom-scroll rounded-lg space-y-2 flex flex-col items-center w-full">
+          <p className=" text-center text-xs font-bold">Ingredientes:</p>
+          <p className=" text-center text-xs font-light">
+            {separateArrayByComma(info.ingredients)}
+          </p>
+        </div>
+        <div className=" w-full flex space-x-2 space-y-2 flex-wrap justify-center">
+          <p className=" text-left text-xs font-bold">
+            Tamaño: <span className=" font-light">{info.size}</span>{" "}
+          </p>
+          <p className=" text-left text-xs font-bold">
+            Cantidad: <span className=" font-light">{info.quantity}</span>{" "}
+          </p>
+          <p className=" text-left text-xs font-bold">
+            Precio: <span className=" font-light">${info.finalPrice}MXN</span>{" "}
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default OrderPizzaItem;
