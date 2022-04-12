4e0cc13e0258e80f1cac0075898f5176b4e84348
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-12 14:09:48 -0500

    :adhesive_bandage: update rounding order pizza items container

--- src/pages/OrderPizza/OrderPizza.jsx
+++ src/pages/OrderPizza/OrderPizza.jsx
@@ -14,12 +14,12 @@ const OrderPizza = () => {
 
 
   return (
-    <div className="p-5 h-[85vh] bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
+    <div className=" min-w-[320px] p-5 h-[85vh] bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
       <h2 className=" text-4xl md:text-7xl lg:text-6xl font-bold text-center text-white">
         Revisa tu pedido
       </h2>
-      <div className=" w-full lg:w-[60%] relative h-[60vh] bg-white-glass border-2 border-blue rounded-lg  ">
-        <div className=" h-[58vh] space-y-2 overflow-y-scroll p-2" >
+      <div className=" w-full lg:w-[60%] border-2 rounded-l-lg rounded-none relative h-[60vh] bg-white-glass border-blue   ">
+        <div className=" custom-scroll2 h-[58vh] space-y-2 overflow-y-scroll p-2" >
           {shoppingCartState.map((element, index) => (
             <OrderPizzaItem key={index} info={element} />
           ))}
