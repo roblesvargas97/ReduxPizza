757f8766d8905b6fd62876a8eae945cdd91e35b7
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-12 16:12:30 -0500

    :construction: work in progress  PurchaseForm

--- src/pages/PurchaseForm/InputForm/InputForm.jsx
+++ src/pages/PurchaseForm/InputForm/InputForm.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-const InputForm = ({ handler, idInput, labelText, typeInput }) => {
+const InputForm = ({
+  handler,
+  handlerState,
+  idInput,
+  labelText,
+  typeInput,
+}) => {
   return (
     <div className="relative">
       <input
         onChange={(e) => handler(e)}
-        className=" rounded py-1 text-white text-xl border-2 border-blue bg-transparent"
+        className=" rounded py-2 px-2 text-white text-xl bg-gray-800 focus:outline-none ring-1 ring-blue focus:ring-4 focus:ring-blue transition-all"
         type={typeInput}
         id={idInput}
       />
       <label
-        className=" absolute top-0 left-0 text-xl text-white bg-white-glass font-bold "
+        className={`absolute top-2 left-2 text-white font-bold transition-all ${
+          handlerState === ""
+            ? " text-xl translate-y-0"
+            : " text-[10px] -translate-y-2"
+        }`}
         htmlFor={idInput}
       >
         {labelText}
--- src/pages/PurchaseForm/PurchaseForm.jsx
+++ src/pages/PurchaseForm/PurchaseForm.jsx
@@ -3,26 +3,102 @@ import InputForm from "./InputForm/InputForm";
 
 const PurchaseForm = () => {
   const [name, setName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [numberPhone, setNumberPhone] = React.useState("");
+  const [direction, setDirection] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [cp, setCp] = React.useState("");
 
   const handleName = (e) => {
     setName(e.target.value);
   };
 
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleNumberPhone = (e) => {
+    setNumberPhone(e.target.value);
+  };
+
+  const handleDirection = (e) => {
+    setDirection(e.target.value);
+  };
+
+  const handleCity = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleCp = (e) => {
+    setCp(e.target.value);
+  };
+
   return (
-    <div className="min-w-[320px] p-5 h-[85vh] bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
+    <div className="min-w-[320px] p-5 min-h-[85vh]  bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
       <h1 className="text-4xl md:text-7xl lg:text-6xl font-bold text-center text-white">
         Formulario de Compra
       </h1>
-      <form className=" p-2 w-full h-auto bg-white-glass">
-        <div className=" h-44">
-          <h2 className=" text-2xl font-bold text-white">
-            Informacion del Comprador
+      <form className=" rounded-lg border-2 border-blue p-2 w-full h-auto bg-white-glass">
+        <div className=" flex flex-col items-center space-y-4">
+          <h2 className=" text-2xl text-center font-bold text-white">
+            Informacion
           </h2>
           <InputForm
             handler={handleName}
-            idInput={name}
+            handlerState={name}
+            idInput={"name"}
             labelText={"Nombre"}
             typeInput={"text"}
+          />
+          <InputForm
+            handler={handleLastName}
+            handlerState={lastName}
+            idInput={"lastName"}
+            labelText={"Apellidos"}
+            typeInput={"text"}
+          />
+          <InputForm
+            handler={handleEmail}
+            handlerState={email}
+            idInput={"email"}
+            labelText={"Correo Electronico"}
+            typeInput={"email"}
+          />
+          <InputForm
+            handler={handleNumberPhone}
+            handlerState={numberPhone}
+            idInput={"telephone"}
+            labelText={"Telefono"}
+            typeInput={"number"}
+          />
+          <h2 className=" text-2xl text-center font-bold text-white">
+            Dirección
+          </h2>
+          <InputForm
+            handler={handleDirection}
+            handlerState={direction}
+            idInput={"direction"}
+            labelText={"Direccion"}
+            typeInput={"text"}
+          />
+          <InputForm
+            handler={handleCity}
+            handlerState={city}
+            idInput={"city"}
+            labelText={"Ciudad"}
+            typeInput={"text"}
+          />
+          <InputForm
+            handler={handleCp}
+            handlerState={cp}
+            idInput={"cp"}
+            labelText={"Codigo Postal"}
+            typeInput={"number"}
           />
         </div>
       </form>
