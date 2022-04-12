452b78f75475957548cfabbe8d500db8ecdc602c
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-11 20:46:49 -0500

    :lipstick: fix Ornament position

--- src/pages/PizzaMenu/PizzaMenu.jsx
+++ src/pages/PizzaMenu/PizzaMenu.jsx
@@ -7,11 +7,11 @@ const PizzaMenu = () => {
   return (
     <div className=" h-[85vh] overflow-hidden flex flex-col lg:flex-row lg:justify-between lg:items-center relative p-5 bg-gradient-to-r space-y-5 from-dark-blue via-dark-blue to-dark-blue-1" id="menu">
       <SwirlOrnament
-        styles=" hidden opacity-90 lg:block absolute w-96 top-[-60%] left-[-15%] "
+        styles=" hidden opacity-90 md:block absolute md:h-48 md:w-48 md:top-[-96px] md:left-[-96px] lg:h-96 lg:w-96 lg:top-[-192px] lg:left-[-192px] "
         fillColor="white"
       />
       <PointsOrnament
-        styles=" hidden opacity-90 lg:block absolute w-40 bottom-[-9%] left-0 "
+        styles=" hidden opacity-90 md:block lg:h-52 lg:w-52 absolute md:h-48 md:w-48 md:bottom-[10px] md:right-[10px] lg:bottom-[10px] lg:left-0 "
         fillColor="white"
       />
       <h2 className=" text-4xl md:text-7xl font-bold text-white text-center ">
