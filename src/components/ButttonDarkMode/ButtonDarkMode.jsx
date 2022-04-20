bc43e0dd749d3b836b36e134f348c70fc4e464e7
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-19 20:07:16 -0500

    :dizzy: Add ButtonDarkModeAnimation

--- src/components/ButttonDarkMode/ButtonDarkMode.jsx
+++ src/components/ButttonDarkMode/ButtonDarkMode.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { setDarkMode } from "../../Slices/uiSlice";
+import { useSelector, useDispatch } from "react-redux";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const ButtonDarkMode = () => {
+  const dispatch = useDispatch();
+  const darkModeState = useSelector((state) => state.ui.darkMode);
+
+  const onSetDarkMode = () => {
+    dispatch(setDarkMode(!darkModeState));
+  };
+
   return (
-    <button className="l w-14 h-7 p-1 flex items-center border-2 rounded-lg bg-white-glass backdrop-blur-sm border-blue justify-between relative cursor-pointer transition-all duration-300 ease-in-out shadow-2xl ">
+    <button
+      onClick={onSetDarkMode}
+      className="l w-14 h-7 p-1 flex items-center border-2 rounded-lg bg-white-glass backdrop-blur-sm border-blue justify-between relative cursor-pointer transition-all duration-300 ease-in-out shadow-2xl "
+    >
       <FaSun className=" text-white" />
       <FaMoon className=" text-white" />
-      <span className=" absolute w-7 h-7 bg-white top-[-2px] left-[-2px] rounded-lg" />
+      <span
+        className={` ${
+          !darkModeState ? "translate-x-0" : " translate-x-[100%]"
+        } absolute w-7 h-7 bg-white top-[-2px] left-[-2px] rounded-lg transition-all ease-in-out duration-300`}
+      />
     </button>
   );
 };
