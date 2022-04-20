b42b4aeab64a187d87b9fe4c4fb933fabb8e92d7
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-19 20:33:26 -0500

    :sparkles: Dark Mode Styles

--- src/components/InfoItem/InfoItem.jsx
+++ src/components/InfoItem/InfoItem.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { FaPizzaSlice ,FaHandPointRight , FaMotorcycle } from 'react-icons/fa';
+import React from "react";
+import { useSelector } from "react-redux";
+import { FaPizzaSlice, FaHandPointRight, FaMotorcycle } from "react-icons/fa";
 
-const InfoItem = ({icon , text}) => {
-    return (
-        <article
-
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="dark"
-            className=' w-full md:w-80 h-60 lg:h-52 cursor-pointer  text-gray-500 flex flex-col items-center justify-between border-2 border-gray-500  shadow-md lg:border-gray-400 lg:text-gray-400 lg:hover:text-gray-500 lg:hover:border-gray-500 shadow-gray-200 p-10 rounded-lg hover:shadow-xl hover:shadow-gray-500  transition-all ' >
-            {icon==='FaPizzaSlice' && <FaPizzaSlice className=' text-7xl' /> }
-            {icon==='FaHandPointRight' && <FaHandPointRight className=' text-7xl' /> }
-            {icon==='FaMotorcycle' && <FaMotorcycle className=' text-7xl' /> }
-            <p className=' text-center text-2xl lg:text-xl font-bold' >{text}</p>
-        </article>
-    );
-}
+const InfoItem = ({ icon, text }) => {
+  const darkModeState = useSelector((state) => state.ui.darkMode);
+  return (
+    <article
+      data-mdb-ripple="true"
+      data-mdb-ripple-color={`${!darkModeState ? "dark" : "light"}`}
+      className={` ${
+        !darkModeState
+          ? " lg:border-gray-400 lg:text-gray-400 lg:hover:text-gray-500 lg:hover:border-gray-500 text-gray-500 border-gray-500 shadow-gray-200 shadow-md hover:shadow-gray-500 hover:shadow-xl  "
+          : "text-white border-white shadow-slate-100 shadow-sm hover:shadow-slate-100 hover:shadow-md "
+      } w-full md:w-80 h-60 lg:h-52 cursor-pointer flex flex-col items-center justify-between border-2 p-10 rounded-lg  transition-all `}
+    >
+      {icon === "FaPizzaSlice" && <FaPizzaSlice className=" text-7xl" />}
+      {icon === "FaHandPointRight" && (
+        <FaHandPointRight className=" text-7xl" />
+      )}
+      {icon === "FaMotorcycle" && <FaMotorcycle className=" text-7xl" />}
+      <p className=" text-center text-2xl lg:text-xl font-bold">{text}</p>
+    </article>
+  );
+};
 
 export default InfoItem;
