908285b3ca5b365439d51bf7b04d6ad2f67616df
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-22 14:54:02 -0500

    :memo: changes

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className=' flex flex-col items-center justify-center h-[10vh] bg-gradient-to-tr from-sky-500 via-sky-200' >
-            <h3 className=' text-dark-blue text-xl font-bold'>Aplicacion Desarrollada por Jesús Uriel Robles Vargas</h3>
+        <footer className=' flex flex-col items-center justify-center h-auto py-2 px-5 bg-gradient-to-tr from-sky-500 via-sky-200' >
+            <h3 className=' text-dark-blue text-xl text-cente font-bold'>Aplicacion Desarrollada por Jesús Uriel Robles Vargas</h3>
             <span className=' text-2xl' >😃🤓🤟👨🏽‍💻🛸🚀🧡</span>
         </footer>
     );
