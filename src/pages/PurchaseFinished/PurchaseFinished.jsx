dc51a67492646e3e8c7a497a962127a540ed9fa8
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-22 13:30:34 -0500

    :sparkles: Add New Page

--- src/pages/PurchaseFinished/PurchaseFinished.jsx
+++ src/pages/PurchaseFinished/PurchaseFinished.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { BsFillEmojiLaughingFill } from "react-icons/bs";
+import { setInfoClient } from "../../Slices/pizzaSlice";
+import { useNavigate } from "react-router-dom";
 
 const PurchaseFinished = () => {
+  const dispatch = useDispatch();
+  const history = useNavigate();
+
+  const [currentClientInfo, setCurrentClientInfo] = React.useState("");
+  const clientInfo = useSelector((state) => state.pizza.infoOrderClient);
+
+  React.useEffect(() => {
+    if (clientInfo.name === undefined) {
+      history("/");
+    }
+
+    setCurrentClientInfo(clientInfo);
+    dispatch(setInfoClient({}));
+  }, []);
+
   return (
     <div className=" relative min-w-[320px] p-5 min-h-[85vh]  bg-gradient-to-r from-dark-blue via-dark-blue to-dark-blue-1 flex flex-col justify-center items-center space-y-5">
-      <h2 className=" text-4xl md:text-7xl lg:text-6xl font-bold text-center text-white">
-        Tu Pedido esta en camino
-      </h2>
+      <div className=" w-full lg:w-[50%] flex flex-col justify-center items-center space-y-10">
+        <h2 className=" text-4xl md:text-7xl lg:text-6xl font-bold text-center text-white">
+          {`!Gracias¡ ${currentClientInfo.name} tu pedido esta en camino. `}
+        </h2>
+        <BsFillEmojiLaughingFill size={"150px"} color="#fff" />
+      </div>
     </div>
   );
 };
